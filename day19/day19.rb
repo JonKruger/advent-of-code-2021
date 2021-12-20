880474eeea3cd7667a894f793984df519d658661
Jon Kruger <84333+JonKruger@users.noreply.github.com>
2021-12-20 08:48:04 -0500

    works all the way now

--- day19/day19.rb
+++ day19/day19.rb
@@ -16,16 +16,6 @@ class Beacon
     @location.size
   end
 
-  def distances_to_neighbors
-    @neighbors.map { |neighbor| distance_to(neighbor) }.sort
-  end
-
-  def distance_to(other)
-    # determine distance to other point using Pythagorean theorem
-    # direction doesn't matter here
-    Math.sqrt((0...location.size).map { |i|  (location[i] - other.location[i]) ** 2 }.sum).round(4)
-  end
-
   def transform_to(other, coordinate_transform = nil)
     if coordinate_transform
       raise TypeError unless coordinate_transform.is_a?(Transform)
@@ -59,7 +49,7 @@ end
 
 class Scanner
   attr_reader :beacons, :name, :beacon_array, :num_dimensions
-  attr_accessor :location
+  attr_accessor :location, :common_beacon_count
 
   def initialize(beacon_array = [], common_beacon_count = 2, name: nil, num_dimensions: 3)
     @beacon_array = beacon_array
@@ -150,179 +140,8 @@ def unique_beacons(scanners)
       (0...b.dimensions).map { |i| b.location[i] + scanner.location[i] }
     end
   end
-  pp(beacons.tally)
   beacons.uniq.size
 end
-
-# simple example
-s0 = Scanner.new([[0,2,0],[4,1,0],[3,3,0]], 3).scan
-s1 = Scanner.new([[-1,-1,0],[-5,0,0],[-2,1,0]], 3).scan
-raise unless s0.beacons.map { |b| b.distances_to_neighbors }.sort == s1.beacons.map { |b| b.distances_to_neighbors }.sort
-
-transform, s1_location = s1.location_relative_to(s0)
-raise s1_location.inspect unless s1_location == [5,2,0]
-
-# larger example
-s0_beacons = [
-  [404,-588,-901],
-  [528,-643,409],
-  [-838,591,734],
-  [390,-675,-793],
-  [-537,-823,-458],
-  [-485,-357,347],
-  [-345,-311,381],
-  [-661,-816,-575],
-  [-876,649,763],
-  [-618,-824,-621],
-  [553,345,-567],
-  [474,580,667],
-  [-447,-329,318],
-  [-584,868,-557],
-  [544,-627,-890],
-  [564,392,-477],
-  [455,729,728],
-  [-892,524,684],
-  [-689,845,-530],
-  [423,-701,434],
-  [7,-33,-71],
-  [630,319,-379],
-  [443,580,662],
-  [-789,900,-551],
-  [459,-707,401]
-]
-
-s1_beacons = [
-  [686,422,578],
-  [605,423,415],
-  [515,917,-361],
-  [-336,658,858],
-  [95,138,22],
-  [-476,619,847],
-  [-340,-569,-846],
-  [567,-361,727],
-  [-460,603,-452],
-  [669,-402,600],
-  [729,430,532],
-  [-500,-761,534],
-  [-322,571,750],
-  [-466,-666,-811],
-  [-429,-592,574],
-  [-355,545,-477],
-  [703,-491,-529],
-  [-328,-685,520],
-  [413,935,-424],
-  [-391,539,-444],
-  [586,-435,557],
-  [-364,-763,-893],
-  [807,-499,-711],
-  [755,-354,-619],
-  [553,889,-390]
-]
-
-s2_beacons = [
-  [649,640,665],
-  [682,-795,504],
-  [-784,533,-524],
-  [-644,584,-595],
-  [-588,-843,648],
-  [-30,6,44],
-  [-674,560,763],
-  [500,723,-460],
-  [609,671,-379],
-  [-555,-800,653],
-  [-675,-892,-343],
-  [697,-426,-610],
-  [578,704,681],
-  [493,664,-388],
-  [-671,-858,530],
-  [-667,343,800],
-  [571,-461,-707],
-  [-138,-166,112],
-  [-889,563,-600],
-  [646,-828,498],
-  [640,759,510],
-  [-630,509,768],
-  [-681,-892,-333],
-  [673,-379,-804],
-  [-742,-814,-386],
-  [577,-820,562]
-]
-
-s3_beacons = [
-  [-589,542,597],
-  [605,-692,669],
-  [-500,565,-823],
-  [-660,373,557],
-  [-458,-679,-417],
-  [-488,449,543],
-  [-626,468,-788],
-  [338,-750,-386],
-  [528,-832,-391],
-  [562,-778,733],
-  [-938,-730,414],
-  [543,643,-506],
-  [-524,371,-870],
-  [407,773,750],
-  [-104,29,83],
-  [378,-903,-323],
-  [-778,-728,485],
-  [426,699,580],
-  [-438,-605,-362],
-  [-469,-447,-387],
-  [509,732,623],
-  [647,635,-688],
-  [-868,-804,481],
-  [614,-800,639],
-  [595,780,-596]
-]
-
-s4_beacons = [
-  [727,592,562],
-  [-293,-554,779],
-  [441,611,-461],
-  [-714,465,-776],
-  [-743,427,-804],
-  [-660,-479,-426],
-  [832,-632,460],
-  [927,-485,-438],
-  [408,393,-506],
-  [466,436,-512],
-  [110,16,151],
-  [-258,-428,682],
-  [-393,719,612],
-  [-211,-452,876],
-  [808,-476,-593],
-  [-575,615,604],
-  [-485,667,467],
-  [-680,325,-822],
-  [-627,-443,-432],
-  [872,-547,-609],
-  [833,512,582],
-  [807,604,487],
-  [839,-516,451],
-  [891,-625,532],
-  [-652,-548,-490],
-  [30,-46,-14]
-]
-
-s0 = Scanner.new(s0_beacons, 12, name: 0).scan
-s1 = Scanner.new(s1_beacons, 12, name: 1).scan
-s2 = Scanner.new(s2_beacons, 12, name: 2).scan
-s3 = Scanner.new(s3_beacons, 12, name: 3).scan
-s4 = Scanner.new(s4_beacons, 12, name: 4).scan
-
-s0.location = [0,0,0]
-known_scanners = [s0]
-unknown_scanners = [s1, s2, s3, s4]
-
-match_scanners(known_scanners, unknown_scanners)
-raise s0.location.inspect unless s0.location == [0, 0, 0]
-raise s1.location.inspect unless s1.location == [68,-1246,-43]
-raise s2.location.inspect unless s2.location == [1105,-1205,1229]
-raise s3.location.inspect unless s3.location == [-92,-2380,-20]
-raise s4.location.inspect unless s4.location == [-20,-1133,1061]
-
-raise unless unique_beacons([s0, s1, s2, s3, s4]) == 79
 
 def parse(input)
   scanners = []
@@ -344,10 +163,33 @@ def parse(input)
   scanners
 end
 
-scanners = parse(File.read("input.txt"))
-raise scanners.size.inspect unless scanners.size == 29
+# simple example
+s0 = Scanner.new([[0,2,0],[4,1,0],[3,3,0]], 3).scan
+s1 = Scanner.new([[-1,-1,0],[-5,0,0],[-2,1,0]], 3).scan
+
+transform, s1_location = s1.location_relative_to(s0)
+raise s1_location.inspect unless s1_location == [5,2,0]
+
+# larger example
+scanners = parse(File.read("test_input.txt"))
+scanners.each { |s| s.common_beacon_count = 12 }
+scanners[0].location = [0,0,0]
+known_scanners = [scanners[0]]
+unknown_scanners = scanners[1..]
+
+match_scanners(known_scanners, unknown_scanners)
+raise scanners[0].location.inspect unless scanners[0].location == [0, 0, 0]
+raise scanners[1].location.inspect unless scanners[1].location == [68,-1246,-43]
+raise scanners[2].location.inspect unless scanners[2].location == [1105,-1205,1229]
+raise scanners[3].location.inspect unless scanners[3].location == [-92,-2380,-20]
+raise scanners[4].location.inspect unless scanners[4].location == [-20,-1133,1061]
+
+raise unless unique_beacons(scanners) == 79
 
 puts("start part 1")
+scanners = parse(File.read("input.txt"))
+raise scanners.size.inspect unless scanners.size == 29
+scanners.each { |s| s.common_beacon_count = 12 }
 scanners[0].location = [0,0,0]
 known_scanners = [scanners[0]]
 unknown_scanners = scanners[1..]
@@ -356,3 +198,9 @@ match_scanners(known_scanners, unknown_scanners)
 
 puts("part 1 - #{unique_beacons(scanners)}")
 
+manhattan_distance = scanners.product(scanners).map do |one, two|
+  (one.location[0] - two.location[0]).abs +
+    (one.location[1] - two.location[1]).abs +
+    (one.location[2] - two.location[2]).abs
+end.max
+puts("part 2 - #{manhattan_distance }")
